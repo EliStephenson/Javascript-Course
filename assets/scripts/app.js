2c5d6a2a559da001d0772a03c09a67219b4ecff3
EliStephenson <elistephenson17@gmail.com>
2023-01-16 16:27:05 -0700

    added button for additon functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,18 +1,19 @@
 const defaultResult = 0;
 let currentResult = defaultResult; // using a semicolon at the end is optional but can be good practice for other languages
 
-const addResult = addNums(1, 4); // calls the function
+addBtn.addEventListener('click', addNums);
+
 
 //let calcDescription = '( ' + defaultResult + ' + 10) * 3 / 2 - 1'; NORMAL CONCATENATION WAY TO DO THIS
-let calcDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+//let calcDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
-outputResult(addResult, calcDescription); // calls function in vendor script
 
-function addNums(num1, num2) {
+
+function addNums() {
   // defining a function
-  const result = num1 + num2;
-  alert(`the result is ${result}`);
+  currentResult = currentResult + userInput.value;
+  //alert(`the result is ${result}`);
   // alert('the result is' + result); either works
-  return result;
+  outputResult(addResult, calcDescription); // outputs result onto page when addNums function runs
 }
 // use alt + shift + s to use prettier
