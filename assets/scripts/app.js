9f0c28e56317bbbf1ce437b02b21e5707adc3ff2
EliStephenson <elistephenson17@gmail.com>
2023-01-25 20:45:20 -0700

    added sub, multi, operations

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,17 +3,38 @@ let currentResult = defaultResult; // using a semicolon at the end is optional b
 
 addBtn.addEventListener('click', addNums);
 
-
+function getUserInputNum(){
+return parseInt(userInput.value)
+}
 //let calcDescription = '( ' + defaultResult + ' + 10) * 3 / 2 - 1'; NORMAL CONCATENATION WAY TO DO THIS
 //let calcDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
-
+function writeOutput(operator, resultBeforeCalc, calcNumber){
+  const calcDescription = `${resultBeforeCalc} ${operator} ${enteredNumber}`;
+  outputResult(currentResult, calcDescription);
+}
 
 function addNums() {
-  // defining a function
-  currentResult = currentResult + userInput.value;
-  //alert(`the result is ${result}`);
-  // alert('the result is' + result); either works
-  outputResult(addResult, calcDescription); // outputs result onto page when addNums function runs
+  const enteredNumber = getUserInputNum();
+  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult = currentResult + enteredNumber; // parse int parses as a number insted of a string can also use parseFloat (more specific)
+  //currentResult = currentResult + +userInput.value; // second plus parses as a number insted of a string but is less explicat
+ 
+  outputResult(currentResult, calcDescription); // outputs result onto page when addNums function runs
 }
-// use alt + shift + s to use prettier
+
+function subtractNums() {
+  const enteredNumber = getUserInputNum();
+  const calcDescription = `${currentResult} - ${enteredNumber}`;
+  currentResult = currentResult - enteredNumber;
+
+  outputResult(currentResult, calcDescription);
+}
+
+function multiplyNums() {
+  const enteredNumber = getUserInputNum();
+  const calcDescription = `${currentResult} * ${enteredNumber}`;
+  currentResult = currentResult * enteredNumber;
+
+  outputResult(currentResult, calcDescription);
+}
