fd926da7a0d83ddb9e73fc2c7b334446d0506901
EliStephenson <elistephenson17@gmail.com>
2023-01-26 20:23:50 -0700

    cleaned up code and added logEntries to store entries

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,48 +1,54 @@
 const defaultResult = 0;
 let currentResult = defaultResult; // using a semicolon at the end is optional but can be good practice for other languages
-
-addBtn.addEventListener('click', addNums);
+let logEntries = [];
 
 function getUserInputNum(){
-return parseInt(userInput.value)
+return parseInt(userInput.value);
 }
 //let calcDescription = '( ' + defaultResult + ' + 10) * 3 / 2 - 1'; NORMAL CONCATENATION WAY TO DO THIS
 //let calcDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 function writeOutput(operator, resultBeforeCalc, calcNumber){
-  const calcDescription = `${resultBeforeCalc} ${operator} ${enteredNumber}`;
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
   outputResult(currentResult, calcDescription); // outputs result onto page when addNums function runs
 }
 
 function addNums() {
   const enteredNumber = getUserInputNum();
-  const calcDescription = `${currentResult} + ${enteredNumber}`;
-  currentResult = currentResult + enteredNumber; // parse int parses as a number insted of a string can also use parseFloat (more specific)
+  const initialResult = currentResult;
+  currentResult += enteredNumber; // parse int parses as a number insted of a string can also use parseFloat (more specific)
   //currentResult = currentResult + +userInput.value; // second plus parses as a number insted of a string but is less explicat
  
-  writeOutput('+', currentResult, enteredNumber);
+  writeOutput('+', initialResult, enteredNumber);
+  logEntries.push(enteredNumber);
+  console.log(logEntries[0]); // zero based arrays
 }
 
 function subtractNums() {
   const enteredNumber = getUserInputNum();
-  const calcDescription = `${currentResult} - ${enteredNumber}`;
-  currentResult = currentResult - enteredNumber;
+  const initialResult = currentResult;
+  currentResult -=  enteredNumber;
 
-  writeOutput('-', currentResult, enteredNumber);
+  writeOutput('-', initialResult, enteredNumber);
 }
 
 function multiplyNums() {
   const enteredNumber = getUserInputNum();
-  const calcDescription = `${currentResult} * ${enteredNumber}`;
-  currentResult = currentResult * enteredNumber;
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
 
-  writeOutput('*', currentResult, enteredNumber);
+  writeOutput('*', initialResult, enteredNumber);
 }
 
 function divideNums() {
   const enteredNumber = getUserInputNum();
-  const calcDescription = `${currentResult} / ${enteredNumber}`;
-  currentResult = currentResult / enteredNumber;
+  const initialResult = currentResult;
+  currentResult /= enteredNumber; 
 
-  writeOutput('/', currentResult, enteredNumber);
+  writeOutput('/', initialResult, enteredNumber);
 }
+
+addBtn.addEventListener('click', addNums);
+subtractBtn.addEventListener('click', subtractNums);
+multiplyBtn.addEventListener('click', multiplyNums);
+divideBtn.addEventListener('click', divideNums);
