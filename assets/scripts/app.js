60ec8c49e29c3da07d451bea7881142fdb666884
EliStephenson <elistephenson17@gmail.com>
2023-01-28 20:00:34 -0700

    added logEntry object for addition

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,13 +2,13 @@ const defaultResult = 0;
 let currentResult = defaultResult; // using a semicolon at the end is optional but can be good practice for other languages
 let logEntries = [];
 
-function getUserInputNum(){
-return parseInt(userInput.value);
+function getUserInputNum() {
+  return parseInt(userInput.value);
 }
 //let calcDescription = '( ' + defaultResult + ' + 10) * 3 / 2 - 1'; NORMAL CONCATENATION WAY TO DO THIS
 //let calcDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
-function writeOutput(operator, resultBeforeCalc, calcNumber){
+function writeOutput(operator, resultBeforeCalc, calcNumber) {
   const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
   outputResult(currentResult, calcDescription); // outputs result onto page when addNums function runs
 }
@@ -18,16 +18,23 @@ function addNums() {
   const initialResult = currentResult;
   currentResult += enteredNumber; // parse int parses as a number insted of a string can also use parseFloat (more specific)
   //currentResult = currentResult + +userInput.value; // second plus parses as a number insted of a string but is less explicat
- 
+
   writeOutput('+', initialResult, enteredNumber);
-  logEntries.push(enteredNumber);
-  console.log(logEntries[0]); // zero based arrays
+  const logEntry = {
+    operation: 'ADD',
+    prevResult: initialResult,
+    number: enteredNumber,
+    result: currentResult
+  }; // creating an object
+  logEntries.push(logEntry); // pushed enteredNumber into an array to keep track of it 
+  console.log(logEntry.operation);
+  console.log(logEntries); // zero based arrays
 }
 
 function subtractNums() {
   const enteredNumber = getUserInputNum();
   const initialResult = currentResult;
-  currentResult -=  enteredNumber;
+  currentResult -= enteredNumber;
 
   writeOutput('-', initialResult, enteredNumber);
 }
@@ -43,7 +50,7 @@ function multiplyNums() {
 function divideNums() {
   const enteredNumber = getUserInputNum();
   const initialResult = currentResult;
-  currentResult /= enteredNumber; 
+  currentResult /= enteredNumber;
 
   writeOutput('/', initialResult, enteredNumber);
 }
@@ -52,3 +59,4 @@ addBtn.addEventListener('click', addNums);
 subtractBtn.addEventListener('click', subtractNums);
 multiplyBtn.addEventListener('click', multiplyNums);
 divideBtn.addEventListener('click', divideNums);
+// alt + shift + f to use prettier
